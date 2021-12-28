7ed9ef0ac3fa31844c51e160a667287c835ade5c
David Henshaw <davidhenshaw2014@gmail.com>
2021-12-28 16:53:31 -0600

    Add UI scene

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSession : MonoBehaviour
+{
+    PlayerController player;
+    Board board;
+
+    public bool isComplete;
+
+
+}
