0ce96857b6cd04b92c7cc9a81d62353ad529d650
David Henshaw <davidhenshaw2014@gmail.com>
2022-10-21 07:35:45 -0500

    set up bee animations

--- Assets/_Scripts/Managers/Singleton.cs
+++ Assets/_Scripts/Managers/Singleton.cs
@@ -30,8 +30,8 @@ public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 
     private void OnApplicationQuit()
     {
-        Instance = null;
         Destroy(gameObject);
+        Instance = null;
     }
 }
 
