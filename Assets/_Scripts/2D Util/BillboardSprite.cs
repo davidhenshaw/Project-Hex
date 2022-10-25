edb6612dde358525b89e67c4e25c5cc01493455b
David Henshaw <davidhenshaw2014@gmail.com>
2022-10-25 08:43:21 -0500

    rename scripts

--- Assets/_Scripts/2D Util/BillboardSprite.cs
+++ Assets/_Scripts/2D Util/BillboardSprite.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BeelineSprite : MonoBehaviour
+public class BillboardSprite : MonoBehaviour
 {
     private Camera activeCamera;
 
